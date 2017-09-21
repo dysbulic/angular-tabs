41b08d59835b2343b47c064b9944986a379498cf
Will Holcomb <will@dhappy.org>
2017-09-21 07:46:39 -0400

    got working after many hours

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 angular.module('tabs', ['ngMaterial', 'ui.router'])
-    .config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        ($stateProvider, $urlRouterProvider) => {
-            $stateProvider
-                .state('home', {
-                    url: '/',
-                    data: {
-                        'selectedTab' : 0
-                    },
-                    templateUrl: 'app/components/main/main.html',
-                    controller: 'MainController as vm'
-                })
-                .state('portfolio', {
-                    url: '/portfolio',
-                    data: {
-                        'selectedTab' : 1
-                    },
-                    templateUrl: 'app/components/portfolio/portfolio.html',
-                    controller: 'PortfolioController as vm'
-                })
-                .state('contact', {
-                    url: '/contact',
-                    data: {
-                        'selectedTab' : 2
-                    },
-                    templateUrl: 'app/components/contact/contact.html',
-                    controller: 'ContactController as vm'
-                });
-        }
-        
-    ])
+.config(['$stateProvider', '$urlRouterProvider',
+  ($stateProvider, $urlRouterProvider) => {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      data: { selectedTab: 0 },
+      template: '<p>HOME</p>',
+    })
+    .state('portfolio', {
+      url: '/portfolio',
+      data: { selectedTab: 1 },
+      template: "<p>'FOLIO</p>",
+    })
+    .state('contact', {
+      url: '/contact',
+      data: { selectedTab: 2 },
+      template: "<p>'TACT</p>",
+    })
+  }
+  ])
+.controller('NavigationController', function($scope, $transitions) {
+  $transitions.onSuccess({},
+    function(transition) {
+      var $state = transition.router.stateService
+      var currentTab = $state.$current.data.selectedTab
+      $scope.selectedTab = currentTab
+    }
+    )
+})
--- navigation.controller.js
+++
@@ -1,9 +0,0 @@
-function NavigationController($scope) {
-
-    var vm = this;
-
-    $scope.$on('$stateChangeSuccess', function(event, toState) {
-        vm.currentTab = toState.data.selectedTab;
-    });
-
-}
